ecf9f7a334c85edeab5b268dc998cd6a7e66592f
taeseokOh <xotjr8052@gmail.com>
2022-02-27 00:40:15 +0900

    onload 시간차 문제 다시 시도

--- src/main/resources/static/js/custom-addorder.js
+++ src/main/resources/static/js/custom-addorder.js
@@ -1,8 +1,13 @@
 /*--------------------------------------add order에만 해당하는 거------------------------------------------*/
-if(document.readyState == "loading"){
+/*if(document.readyState == "loading"){
     let curCategory = document.querySelector("#field-orderform-fields-cur-category").value;
     $('#orderform-category').val(curCategory).trigger("change");
-    /*$('#orderform-category').val(curCategory).prop("selected");*/
+    *//*$('#orderform-category').val(curCategory).prop("selected");*//*
+}*/
+
+document.querySelector("#field-orderform-fields-cur-category").onload = function(){
+    let curCategory = document.querySelector("#field-orderform-fields-cur-category").value;
+    $('#orderform-category').val(curCategory).trigger("change");
 }
 
 window.onload = function(){ //다른걸로 덮어쓰기로 해결
@@ -10,6 +15,14 @@ window.onload = function(){ //다른걸로 덮어쓰기로 해결
     $('#orderform-service').val(curService).trigger("change");
     console.log("현재서비스 : ",curService);
 }
+
+
+
+
+
+
+
+
 
 /*window.addEventListener("load",()=>{
     let curService = document.querySelector("#field-orderform-fields-cur-service").value;
